23e297e2dba7ba4473d56f534230a6e4c3cac755
qwel0rd <secondofall22@rambler.ru>
2025-12-22 02:47:58 +0500

    Разработанные интерфейсы. Лукашевич

---
+++ Interfaces/IContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Shop_Lukashevich.Interfaces
+{
+    public interface IContext
+    {
+        List<object> All();
+        void Save(bool Update = false);
+        void Delete();
+    }
+}
