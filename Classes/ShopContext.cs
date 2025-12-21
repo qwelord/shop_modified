914a5a5508945824dfe41d38a31c0cecbe532efb
qwel0rd <secondofall22@rambler.ru>
2025-12-22 02:56:02 +0500

    Разработанный контекст для детских товаров. Лукашевич

---
+++ Classes/ShopContext.cs
@@ -0,0 +1,52 @@
+﻿using Shop_Lukashevich.Classes.Common;
+using Shop_Lukashevich.Interfaces;
+using Shop_Lukashevich.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+
+namespace Shop_Lukashevich.Classes
+{
+    public class ShopContext : Shop, IContext
+    {
+        public ShopContext()
+        {
+        }
+
+        public ShopContext(int Id, string Name, int Price) : base(Id, Name, Price)
+        {
+        }
+
+        public List<object> All()
+        {
+            List<object> allShop = new List<object>();
+
+            OleDbConnection connection = Common.DBConnect.Connection();
+            OleDbDataReader shopData = Common.DBConnect.Query("SELECT * FROM [Товар]", connection);
+
+            while (shopData.Read())
+            {
+                ShopContext newShop = new ShopContext(
+                    shopData.GetInt32(0),
+                    shopData.GetString(1),
+                    shopData.GetInt32(2)
+                );
+                allShop.Add(newShop);
+            }
+
+            Common.DBConnect.CloseConnection(connection);
+
+            return allShop;
+        }
+
+        public void Delete()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Save(bool Update = false)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- Models/Shop.cs
+++ Models/Shop.cs
@@ -1,20 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Shop_Lukashevich.Models
+﻿namespace Shop_Lukashevich.Classes
 {
     public class Shop
     {
-
+        public int Id { get; set; }
         public string Name { get; set; }
         public int Price { get; set; }
+
+        public Shop()
+        {
+        }
+
         public Shop(string name, int price)
         {
             Name = name;
-           Price = price;
+            Price = price;
+        }
+
+        public Shop(int id, string name, int price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
         }
     }
 }
