7012a4c7ed32c8c72308df3a1769a6326a54aa21
qwel0rd <secondofall22@rambler.ru>
2025-12-22 02:50:31 +0500

    Разработка общего класса. Лукашевич

---
+++ Classes/Common/DBConnection.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.OleDb;
+
+namespace Shop_Lukashevich.Classes.Common
+{
+    public class DBConnect
+    {
+        public static readonly string Path = @"Shop.accdb";
+
+        public static OleDbConnection Connection()
+        {
+            OleDbConnection oleDbConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + Path);
+            oleDbConnection.Open();
+
+            return oleDbConnection;
+        }
+
+        public static OleDbDataReader Query(string Query, OleDbConnection Connection)
+        {
+            return new OleDbCommand(Query, Connection).ExecuteReader();
+        }
+
+        public static void CloseConnection(OleDbConnection Connection)
+        {
+            Connection.Close();
+        }
+    }
+}
